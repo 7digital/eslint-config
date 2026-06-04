4789621d25c42fe163cebfc970b2b8072366dab9
richgled <richard.gledhill@songtradr.com>
2026-06-04 11:54:05 +0100

    Revert "Set valid ecmaVersion (2025)"
    This reverts commit b9a5fb395d0eee5b409e27e3c62e8652bc1a0c67.
    
    Apparently it is valid WTF

--- api-team.js
+++ api-team.js
@@ -10,7 +10,7 @@ export default [
         ...globals.commonjs,
         ...globals.mocha
       },
-      ecmaVersion: 2025,
+      ecmaVersion: 2026,
       sourceType: 'module'
     },
     rules: {
